f321dc77bd430288421f084f9865916447b1aa30
Phil Davis <phil@jankaritech.com>
2020-01-26 12:05:12 +0545

    Implement php-cs-fixer changes

--- lib/Apcu.php
+++ lib/Apcu.php
@@ -1,4 +1,6 @@
-<?php declare (strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace Sabre\Cache;
 
@@ -14,22 +16,23 @@ use Traversable;
  * @author Evert Pot (https://evertpot.com/)
  * @license http://sabre.io/license/
  */
-class Apcu implements CacheInterface {
-
+class Apcu implements CacheInterface
+{
     use MultipleTrait;
 
     /**
      * Fetches a value from the cache.
      *
-     * @param string $key     The unique key of this item in the cache.
-     * @param mixed  $default Default value to return if the key does not exist.
+     * @param string $key     the unique key of this item in the cache
+     * @param mixed  $default default value to return if the key does not exist
      *
      * @throws \Psr\SimpleCache\InvalidArgumentException
-     *   MUST be thrown if the $key string is not a legal value.
-     * @return mixed The value of the item from the cache, or $default in case of cache miss.
+     *                                                   MUST be thrown if the $key string is not a legal value
+     *
+     * @return mixed the value of the item from the cache, or $default in case of cache miss
      */
-    function get($key, $default = null) {
-
+    public function get($key, $default = null)
+    {
         if (!is_string($key)) {
             throw new InvalidArgumentException('$key must be a string');
         }
@@ -38,29 +41,30 @@ class Apcu implements CacheInterface {
         if (!$success) {
             return $default;
         }
-        return $value;
 
+        return $value;
     }
 
     /**
      * Persists data in the cache, uniquely referenced by a key with an
      * optional expiration TTL time.
      *
-     * @param string                $key   The key of the item to store.
-     * @param mixed                 $value The value of the item to store, must
-     *                                     be serializable.
-     * @param null|int|DateInterval $ttl   Optional. The TTL value of this item.
+     * @param string                $key   the key of the item to store
+     * @param mixed                 $value the value of the item to store, must
+     *                                     be serializable
+     * @param int|DateInterval|null $ttl   Optional. The TTL value of this item.
      *                                     If no value is sent and the driver
      *                                     supports TTL then the library may set
      *                                     a default value for it or let the
      *                                     driver take care of that.
      *
      * @throws \Psr\SimpleCache\InvalidArgumentException
-     *   MUST be thrown if the $key string is not a legal value.
-     * @return bool True on success and false on failure.
+     *                                                   MUST be thrown if the $key string is not a legal value
+     *
+     * @return bool true on success and false on failure
      */
-    function set($key, $value, $ttl = null) {
-
+    public function set($key, $value, $ttl = null)
+    {
         if (!is_string($key)) {
             throw new InvalidArgumentException('$key must be a string');
         }
@@ -68,37 +72,37 @@ class Apcu implements CacheInterface {
             // Converting to a TTL in seconds
             $ttl = (new DateTime('now'))->add($ttl)->getTimeStamp() - time();
         }
-        return apcu_store($key, $value, (int)$ttl);
 
+        return apcu_store($key, $value, (int) $ttl);
     }
 
     /**
      * Delete an item from the cache by its unique key.
      *
-     * @param string $key The unique cache key of the item to delete.
+     * @param string $key the unique cache key of the item to delete
      *
      * @throws \Psr\SimpleCache\InvalidArgumentException
-     *   MUST be thrown if the $key string is not a legal value.
+     *                                                   MUST be thrown if the $key string is not a legal value
+     *
      * @return bool True if the item was successfully removed. False if there was an error.
      */
-    function delete($key) {
-
+    public function delete($key)
+    {
         if (!is_string($key)) {
             throw new InvalidArgumentException('$key must be a string');
         }
-        return apcu_delete($key);
 
+        return apcu_delete($key);
     }
 
     /**
      * Wipes clean the entire cache's keys.
      *
-     * @return bool True on success and false on failure.
+     * @return bool true on success and false on failure
      */
-    function clear() {
-
+    public function clear()
+    {
         return apcu_clear_cache();
-
     }
 
     /**
@@ -110,38 +114,41 @@ class Apcu implements CacheInterface {
      * has() will return true and immediately after, another script can remove
      * it making the state of your app out of date.
      *
-     * @param string $key The cache item key.
+     * @param string $key the cache item key
+     *
      * @throws \Psr\SimpleCache\InvalidArgumentException
-     *   MUST be thrown if the $key string is not a legal value.
+     *                                                   MUST be thrown if the $key string is not a legal value
+     *
      * @return bool
      */
-    function has($key) {
-
+    public function has($key)
+    {
         if (!is_string($key)) {
             throw new InvalidArgumentException('$key must be a string');
         }
-        return apcu_exists($key);
 
+        return apcu_exists($key);
     }
 
     /**
      * Persists a set of key => value pairs in the cache, with an optional TTL.
      *
-     * @param iterable              $values A list of key => value pairs for a
-     *                                      multiple-set operation.
-     * @param null|int|DateInterval $ttl    Optional. The TTL value of this
+     * @param iterable              $values a list of key => value pairs for a
+     *                                      multiple-set operation
+     * @param int|DateInterval|null $ttl    Optional. The TTL value of this
      *                                      item. If no value is sent and the
      *                                      driver supports TTL then the library
      *                                      may set a default value for it or
      *                                      let the driver take care of that.
      *
      * @throws \Psr\SimpleCache\InvalidArgumentException
-     *   MUST be thrown if $values is neither an array nor a Traversable,
-     *   or if any of the $values are not a legal value.
-     * @return bool True on success and false on failure.
+     *                                                   MUST be thrown if $values is neither an array nor a Traversable,
+     *                                                   or if any of the $values are not a legal value
+     *
+     * @return bool true on success and false on failure
      */
-    function setMultiple($values, $ttl = null) {
-
+    public function setMultiple($values, $ttl = null)
+    {
         if (!is_array($values) && !$values instanceof Traversable) {
             throw new InvalidArgumentException('$values must be traversable');
         }
@@ -150,28 +157,28 @@ class Apcu implements CacheInterface {
             // Converting to a TTL in seconds
             $ttl = (new DateTime('now'))->add($ttl)->getTimeStamp() - time();
         }
-    
+
         if ($values instanceof Traversable) {
             $values = iterator_to_array($values);
         }
 
-        return apcu_store($values, null, (int)$ttl);
-
+        return apcu_store($values, null, (int) $ttl);
     }
 
     /**
      * Deletes multiple cache items in a single operation.
      *
-     * @param iterable $keys A list of string-based keys to be deleted.
+     * @param iterable $keys a list of string-based keys to be deleted
      *
      * @throws \Psr\SimpleCache\InvalidArgumentException
-     *   MUST be thrown if $keys is neither an array nor a Traversable,
-     *   or if any of the $keys are not a legal value.
+     *                                                   MUST be thrown if $keys is neither an array nor a Traversable,
+     *                                                   or if any of the $keys are not a legal value
+     *
      * @return bool True if the items were successfully removed. False if there
-     * was an error.
+     *              was an error.
      */
-    function deleteMultiple($keys) {
-
+    public function deleteMultiple($keys)
+    {
         if ($keys instanceof Traversable) {
             $keys = iterator_to_array($keys);
         } elseif (!is_array($keys)) {
@@ -179,6 +186,5 @@ class Apcu implements CacheInterface {
         }
 
         return apcu_delete($keys);
-
     }
 }
--- lib/Memcached.php
+++ lib/Memcached.php
@@ -1,4 +1,6 @@
-<?php declare (strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace Sabre\Cache;
 
@@ -17,17 +19,16 @@ use Traversable;
  * @author Evert Pot (https://evertpot.com/)
  * @license http://sabre.io/license/
  */
-class Memcached implements CacheInterface {
-
+class Memcached implements CacheInterface
+{
     protected $memcached;
 
     /**
      * Creates the PSR-16 Memcache implementation.
      */
-    function __construct(\Memcached $memcached) {
-
+    public function __construct(\Memcached $memcached)
+    {
         $this->memcached = $memcached;
-
     }
 
     use MultipleTrait;
@@ -35,46 +36,47 @@ class Memcached implements CacheInterface {
     /**
      * Fetches a value from the cache.
      *
-     * @param string $key     The unique key of this item in the cache.
-     * @param mixed  $default Default value to return if the key does not exist.
+     * @param string $key     the unique key of this item in the cache
+     * @param mixed  $default default value to return if the key does not exist
      *
      * @throws \Psr\SimpleCache\InvalidArgumentException
-     *   MUST be thrown if the $key string is not a legal value.
-     * @return mixed The value of the item from the cache, or $default in case of cache miss.
+     *                                                   MUST be thrown if the $key string is not a legal value
+     *
+     * @return mixed the value of the item from the cache, or $default in case of cache miss
      */
-    function get($key, $default = null) {
-
+    public function get($key, $default = null)
+    {
         if (!is_string($key)) {
             throw new InvalidArgumentException('$key must be a string');
         }
         $result = $this->memcached->get($key);
-        if ($result === false && $this->memcached->getResultCode() === \Memcached::RES_NOTFOUND) {
+        if (false === $result && \Memcached::RES_NOTFOUND === $this->memcached->getResultCode()) {
             return $default;
         }
 
         return $result;
-
     }
 
     /**
      * Persists data in the cache, uniquely referenced by a key with an
      * optional expiration TTL time.
      *
-     * @param string                $key   The key of the item to store.
-     * @param mixed                 $value The value of the item to store, must
-     *                                     be serializable.
-     * @param null|int|DateInterval $ttl   Optional. The TTL value of this item.
+     * @param string                $key   the key of the item to store
+     * @param mixed                 $value the value of the item to store, must
+     *                                     be serializable
+     * @param int|DateInterval|null $ttl   Optional. The TTL value of this item.
      *                                     If no value is sent and the driver
      *                                     supports TTL then the library may set
      *                                     a default value for it or let the
      *                                     driver take care of that.
      *
      * @throws \Psr\SimpleCache\InvalidArgumentException
-     *   MUST be thrown if the $key string is not a legal value.
-     * @return bool True on success and false on failure.
+     *                                                   MUST be thrown if the $key string is not a legal value
+     *
+     * @return bool true on success and false on failure
      */
-    function set($key, $value, $ttl = null) {
-
+    public function set($key, $value, $ttl = null)
+    {
         if (!is_string($key)) {
             throw new InvalidArgumentException('$key must be a string');
         }
@@ -85,37 +87,37 @@ class Memcached implements CacheInterface {
         } else {
             $expire = 0;
         }
-        return $this->memcached->set($key, $value, $expire);
 
+        return $this->memcached->set($key, $value, $expire);
     }
 
     /**
      * Delete an item from the cache by its unique key.
      *
-     * @param string $key The unique cache key of the item to delete.
+     * @param string $key the unique cache key of the item to delete
      *
      * @throws \Psr\SimpleCache\InvalidArgumentException
-     *   MUST be thrown if the $key string is not a legal value.
+     *                                                   MUST be thrown if the $key string is not a legal value
+     *
      * @return bool True if the item was successfully removed. False if there was an error.
      */
-    function delete($key) {
-
+    public function delete($key)
+    {
         if (!is_string($key)) {
             throw new InvalidArgumentException('$key must be a string');
         }
-        return $this->memcached->delete($key);
 
+        return $this->memcached->delete($key);
     }
 
     /**
      * Wipes clean the entire cache's keys.
      *
-     * @return bool True on success and false on failure.
+     * @return bool true on success and false on failure
      */
-    function clear() {
-
+    public function clear()
+    {
         return $this->memcached->flush();
-
     }
 
     /**
@@ -127,42 +129,45 @@ class Memcached implements CacheInterface {
      * has() will return true and immediately after, another script can remove
      * it making the state of your app out of date.
      *
-     * @param string $key The cache item key.
+     * @param string $key the cache item key
+     *
      * @throws \Psr\SimpleCache\InvalidArgumentException
-     *   MUST be thrown if the $key string is not a legal value.
+     *                                                   MUST be thrown if the $key string is not a legal value
+     *
      * @return bool
      */
-    function has($key) {
-
+    public function has($key)
+    {
         if (!is_string($key)) {
             throw new InvalidArgumentException('$key must be a string');
         }
         $result = $this->memcached->get($key);
-        if ($result === false && $this->memcached->getResultCode() === \Memcached::RES_NOTFOUND) {
+        if (false === $result && \Memcached::RES_NOTFOUND === $this->memcached->getResultCode()) {
             return false;
         }
-        return true;
 
+        return true;
     }
 
     /**
      * Obtains multiple cache items by their unique keys.
      *
-     * This particular implementation returns its result as a generator. 
+     * This particular implementation returns its result as a generator.
      *
-     * @param iterable $keys  A list of keys that can obtained in a single
-     *                        operation.
-     * @param mixed    $default Default value to return for keys that do not
-     *                          exist.
+     * @param iterable $keys    a list of keys that can obtained in a single
+     *                          operation
+     * @param mixed    $default default value to return for keys that do not
+     *                          exist
      *
      * @throws \Psr\SimpleCache\InvalidArgumentException
-     *   MUST be thrown if $keys is neither an array nor a Traversable,
-     *   or if any of the $keys are not a legal value.
+     *                                                   MUST be thrown if $keys is neither an array nor a Traversable,
+     *                                                   or if any of the $keys are not a legal value
+     *
      * @return iterable A list of key => value pairs. Cache keys that do not
      *                  exist or are stale will have $default as value.
      */
-    function getMultiple($keys, $default = null) {
-
+    public function getMultiple($keys, $default = null)
+    {
         if ($keys instanceof Traversable) {
             $keys = iterator_to_array($keys);
         } elseif (!is_array($keys)) {
@@ -175,28 +180,29 @@ class Memcached implements CacheInterface {
                 $result[$key] = $default;
             }
         }
-        return $result;
 
+        return $result;
     }
 
     /**
      * Persists a set of key => value pairs in the cache, with an optional TTL.
      *
-     * @param iterable              $values A list of key => value pairs for a
-     *                                      multiple-set operation.
-     * @param null|int|DateInterval $ttl    Optional. The TTL value of this
+     * @param iterable              $values a list of key => value pairs for a
+     *                                      multiple-set operation
+     * @param int|DateInterval|null $ttl    Optional. The TTL value of this
      *                                      item. If no value is sent and the
      *                                      driver supports TTL then the library
      *                                      may set a default value for it or
      *                                      let the driver take care of that.
      *
      * @throws \Psr\SimpleCache\InvalidArgumentException
-     *   MUST be thrown if $values is neither an array nor a Traversable,
-     *   or if any of the $values are not a legal value.
-     * @return bool True on success and false on failure.
+     *                                                   MUST be thrown if $values is neither an array nor a Traversable,
+     *                                                   or if any of the $values are not a legal value
+     *
+     * @return bool true on success and false on failure
      */
-    function setMultiple($values, $ttl = null) {
-
+    public function setMultiple($values, $ttl = null)
+    {
         if ($values instanceof Traversable) {
             $values = iterator_to_array($values);
         } elseif (!is_array($values)) {
@@ -219,22 +225,22 @@ class Memcached implements CacheInterface {
     /**
      * Deletes multiple cache items in a single operation.
      *
-     * @param iterable $keys A list of string-based keys to be deleted.
+     * @param iterable $keys a list of string-based keys to be deleted
      *
      * @throws \Psr\SimpleCache\InvalidArgumentException
-     *   MUST be thrown if $keys is neither an array nor a Traversable,
-     *   or if any of the $keys are not a legal value.
+     *                                                   MUST be thrown if $keys is neither an array nor a Traversable,
+     *                                                   or if any of the $keys are not a legal value
+     *
      * @return bool True if the items were successfully removed. False if there
-     * was an error.
+     *              was an error.
      */
-    function deleteMultiple($keys) {
-
+    public function deleteMultiple($keys)
+    {
         if ($keys instanceof Traversable) {
             $keys = iterator_to_array($keys);
         } elseif (!is_array($keys)) {
             throw new InvalidArgumentException('$keys must be iterable');
         }
         $this->memcached->deleteMulti($keys);
-
     }
 }
